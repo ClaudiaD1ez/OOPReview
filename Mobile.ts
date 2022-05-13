b33a1a78a7377ac4fe98ee0c44b98053a5adfd52
Claudia Diez <claudia_diez@outlook.com>
2022-05-13 17:02:23 +0200

    Cambios Moviles

--- Mobile.ts
+++ Mobile.ts
@@ -8,8 +8,8 @@ export class Mobile {
     // public cameraNumber:number
     // public price:number
 
-    constructor (public name:string , public model:string , public trademark:string , public sdSize:number ,
-                 public color:string , public is5G:boolean , public cameraNumber:number , public price:number){
+    constructor (private name:string , private model:string , private trademark:string , private sdSize:number ,
+                 private color:string , private is5G:boolean , private cameraNumber:number , private price:number){
                  {
                     this.name = name;
                     this.model = model;
@@ -21,7 +21,74 @@ export class Mobile {
                     this.price = price;
                  }
                  
+      
+
+       }
+
+     //setters y getters
+     public getName():string{
+        return this.name
+    }
+     public setnuevoName(nuevoNombre:string){
+           this.name = nuevoNombre
+     }
+
+     public getModel():string{
+         return this.model
+     }
+     public setnuevoModel(nuevoModelo:string){
+         this.model = nuevoModelo
+     }
+
+     public getTrademark():string{
+         return this.trademark
+     }
+     public setnuevoTrademark(nuevoMarca:string){
+         this.trademark = nuevoMarca
+     }
+
+     public getsdSize():number{
+         return this.sdSize
+     }
+     public setnuevoSdSize(nuevoTamaño:number){
+         this.sdSize = nuevoTamaño
+     }
+
+     public getColor():string{
+         return this.color
+     }
+     public setnuevoColor(nuevoColor:string){
+         this.color = nuevoColor
+     }
+
+     public getIs5G():boolean{
+         return this.is5G
+     }
+     public setnuevoIs5G(nuevo5G:boolean){
+         this.is5G = nuevo5G
+     }
+
+     public getCameraNumber():number{
+         return this.cameraNumber
+     }
+     public setnuevoCameraNumber(nuevoCamera:number){
+         this.cameraNumber = nuevoCamera
+     }
+
+     public getPrice():number{
+         return this.price
+     }
+     public setnuevoPrice(nuevoPrecio:number){
+         this.price = nuevoPrecio
+     }
+
+     //METODOS 
+
+     impCaract(){
+         console.log("The characteristics of the mobile name are: " + "\n" + "name: " + this.name +"\n" + 
+        "model: " + this.model + "/n" + "Trademark: " + this.trademark + "\n" + "SD Size (GB) : " + this.sdSize + "\n" +
+        "color: " + this.color + "\n" + "is5G? " + this.is5G + "\n" + "Number of Cameras: " + this.cameraNumber)
+     }
     
 
-    }
 }
--- MovileTestDos.ts
+++ MovileTestDos.ts
@@ -1,5 +1,7 @@
 import { Mobile } from "./Mobile";
 
+// 6. En otro fichero hacer las mismas operaciones que se hicieron en el punto 2.
+
 let Nokia:Mobile = new Mobile("Nokia 3210", "3210" , "Nokia", 32 , "Gris" , false , 0 , 59 )
 let iPhone:Mobile = new Mobile("iPhone3G","3G","iPhone", 99 , "Negro" , false , 1 , 399 )
 let Sammsung:Mobile = new Mobile("Sammsung Galaxy 10","Galaxy 10","Sammsung", 130 , "Blanco" , true , 3 , 580)
@@ -13,12 +15,26 @@ console.log(Sammsung);
 
 // Modificar el Nokia3210 para que tenga 5G y 4 camaras
 
-//Nokia.is5G =true;
-//Nokia.cameraNumber =4;
+Nokia.setnuevoIs5G(true);
+Nokia.setnuevoCameraNumber(4);
 
 //Mostrar todos los atributos del todos los objetos creados
 
 console.log(Nokia);
 console.log(iPhone);
 console.log(Sammsung);
+
+// 7. Crear un array de objetos de la clase Mobile con los tres objetos ya creados
+
+let moviles:Mobile[] = [Nokia,iPhone,Sammsung];
+
+/* 8. Mostrar cada uno de los atributos de cada uno de los objetos del array creado
+tal y como se especifica en el punto 5.*/
+
+for(let i=0 ; i<moviles.length ; i++){
+     moviles[i].impCaract();
+}
+// Nokia.impCaract();
+// iPhone.impCaract();
+// Sammsung.impCaract();
 
